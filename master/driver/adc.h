fef1812afb105cbc4b81ebbfca6be4d9047a1ce7
hhlinks <2269304391@qq.com>
2025-11-09 15:48:16 +0800

    first commit

--- master/driver/adc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _adc_h_
-#define _adc_h_
-
-#include "stm32f10x.h"
-
-void AdcInit(void);
-void VoltageDetect(void);
-
-#endif
--- master/driver/nvic.h
+++
@@ -1,8 +0,0 @@
-#ifndef _nvic_h_
-#define _nvic_h_
-
-#include "stm32f10x.h"
-
-void NVIC_config(void);
-
-#endif
